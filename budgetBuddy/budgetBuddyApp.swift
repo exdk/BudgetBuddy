b637c4b64da2e2395518f3991f63ad47c639b9a7
Victor <victor@yunusov.su>
2025-09-04 18:55:53 +0300

    Initial Commit

---
+++ budgetBuddy/budgetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetBuddyApp.swift
+//  budgetBuddy
+//
+//  Created by Виктор Юнусов on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct budgetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
